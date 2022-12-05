2e11b120416dd85dd6cbf3b2e7e8dd150ef05d82
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2022-12-05 09:43:58 +0300

    lw7 Доделал Canvas, нарисовал картинку

--- lw/lw7/lib/Canvas/CCanvas/CCanvas.cpp
+++ lw/lw7/lib/Canvas/CCanvas/CCanvas.cpp
@@ -1,7 +1,5 @@
 #include "CCanvas.h"
 
-// TODO: Реализовать!
-
 CCanvas::CCanvas(sf::RenderWindow& window)
 	: m_window(window)
 {
@@ -9,12 +7,70 @@ CCanvas::CCanvas(sf::RenderWindow& window)
 
 void CCanvas::DrawLine(PointD start, PointD end, std::optional<RGBAColor> outline, std::optional<double> thickness)
 {
+	sf::VertexArray line(sf::Quads, 4);
+	sf::Vector2f startVector(start.x, start.y);
+	sf::Vector2f endVector(end.x, end.y);
+
+	sf::Vector2f difference = startVector - endVector;
+	sf::Vector2f perpendicular(-difference.y / difference.x, 1);
+	sf::Vector2f offset = thickness.has_value() ? float(thickness.value() / 2) * perpendicular : perpendicular * float(0);
+	line[0].position = startVector + offset;
+	line[1].position = endVector + offset;
+	line[2].position = endVector - offset;
+	line[3].position = startVector - offset;
+	auto color = outline.has_value() ? outline.value() : 0;
+	line[0].color = sf::Color(color);
+	line[1].color = sf::Color(color);
+	line[2].color = sf::Color(color);
+	line[3].color = sf::Color(color);
+	m_window.draw(line);
 }
 
 void CCanvas::DrawPolygon(const std::vector<PointD>& points, std::optional<RGBAColor> outline, std::optional<RGBAColor> fill, std::optional<double> thickness)
 {
+	sf::ConvexShape shape;
+	shape.setPointCount(points.size());
+
+	for (size_t index = 0; index < points.size(); index++)
+	{
+		shape.setPoint(index, sf::Vector2f((float)points[index].x, (float)points[index].y));
+	}
+
+	if (outline.has_value())
+	{
+		shape.setOutlineColor(sf::Color(outline.value()));
+	}
+	if (thickness.has_value())
+	{
+		shape.setOutlineThickness((float)thickness.value());
+	}
+	if (fill.has_value())
+	{
+		shape.setFillColor(sf::Color(fill.value()));
+	}
+
+	m_window.draw(shape);
 }
 
 void CCanvas::DrawEllipse(PointD center, double horizontalRadius, double verticalRadius, std::optional<RGBAColor> outline, std::optional<RGBAColor> fill, std::optional<double> thickness)
 {
+	sf::CircleShape circleShape((float)horizontalRadius);
+	circleShape.setOrigin((float)horizontalRadius, (float)horizontalRadius);
+	circleShape.move(sf::Vector2f((float)center.x, (float)m_window.getSize().y - (float)center.y));
+	circleShape.setScale(1.f, (float)verticalRadius / (float)horizontalRadius);
+
+	if (outline.has_value())
+	{
+		circleShape.setOutlineColor(sf::Color(outline.value()));
+	}
+	if (thickness.has_value())
+	{
+		circleShape.setOutlineThickness((float)thickness.value());
+	}
+	if (fill.has_value())
+	{
+		circleShape.setFillColor(sf::Color(fill.value()));
+	}
+
+	m_window.draw(circleShape);
 }
--- lw/lw7/main.cpp
+++ lw/lw7/main.cpp
@@ -1,27 +1,88 @@
+#include "lib/Canvas/CCanvas/CCanvas.h"
+#include "lib/GroupShape/CGroupShape/CGroupShape.h"
+#include "lib/Shape/CEllipse/CEllipse.h"
+#include "lib/Shape/CRectangle/CRectangle.h"
+#include "lib/Shape/CTriangle/CTriangle.h"
+#include "lib/Slide/CSlide.h"
 #include <SFML/Graphics.hpp>
+#include <memory>
+
+const int SLIDE_WIDTH = 1200;
+const int SLIDE_HEIGHT = 800;
+
+std::shared_ptr<IShape> CreateBackground()
+{
+	auto sky = std::make_shared<CRectangle>(PointD{ 0, 0 }, SLIDE_WIDTH, SLIDE_HEIGHT);
+	sky->GetFillStyle()->SetColor(0x0000ffff);
+
+	auto grass = std::make_shared<CRectangle>(PointD{ 0, 500 }, SLIDE_WIDTH, SLIDE_HEIGHT - 500);
+	grass->GetFillStyle()->SetColor(0x00ff00ff);
+
+	auto background = std::make_shared<CGroupShape>();
+	background->InsertShape(sky, 0);
+	background->InsertShape(grass, 1);
+
+	return background;
+}
+
+std::shared_ptr<IShape> CreateSun()
+{
+	auto sun = std::make_shared<CEllipse>(PointD{ 1100, 700 }, 50, 50);
+	sun->GetFillStyle()->SetColor(0xffff00ff);
+	return sun;
+}
+
+std::shared_ptr<IShape> CreateHouse()
+{
+	auto main = std::make_shared<CRectangle>(PointD{ 200, 400 }, 200, 200);
+	main->GetFillStyle()->SetColor(0x964b00ff);
+	main->GetOutlineStyle()->SetColor(0xff3867ff);
+	main->GetOutlineStyle()->SetThickness(3);
+
+	auto roof = std::make_shared<CTriangle>(PointD{ 150, 400 }, PointD{ 300, 250 }, PointD{ 450, 400 });
+	roof->GetFillStyle()->SetColor(0x4b4d4bff);
+	roof->GetOutlineStyle()->SetColor(0x000000ff);
+	roof->GetOutlineStyle()->SetThickness(3);
+
+	auto flue = std::make_shared<CRectangle>(PointD{ 350, 250 }, 40, 100);
+	flue->GetFillStyle()->SetColor(0xff0000ff);
+	flue->GetOutlineStyle()->SetColor(0x000000ff);
+	flue->GetOutlineStyle()->SetThickness(5);
+
+	auto result = std::make_shared<CGroupShape>();
+	result->InsertShape(main, 0);
+	result->InsertShape(flue, 1);
+	result->InsertShape(roof, 2);
+
+	return result;
+}
+
+std::shared_ptr<IShape> CreatePond()
+{
+	auto pond = std::make_shared<CEllipse>(PointD{ 800, 150 }, 150, 50);
+	pond->GetFillStyle()->SetColor(0x0000ffff);
+	return pond;
+}
+
+std::shared_ptr<ISlide> CreateSlide()
+{
+	auto slide = std::make_shared<CSlide>(SLIDE_WIDTH, SLIDE_HEIGHT);
+	slide->InsertShape(CreateBackground(), 0);
+	slide->InsertShape(CreateSun(), 1);
+	slide->InsertShape(CreateHouse(), 2);
+	slide->InsertShape(CreatePond(), 3);
+
+	return slide;
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	auto slide = CreateSlide();
+	auto width = slide->GetWidth();
+	auto height = slide->GetHeight();
 
-	sf::VertexArray rect(sf::Quads, 4);
-	double thickness = 20;
-	sf::Vector2f start(10, 10);
-	sf::Vector2f end(150, 150);
-
-	sf::Vector2f difference = start - end;
-	sf::Vector2f perpendicular(-difference.y / difference.x, 1);
-	sf::Vector2f offset = float(thickness / 2) * perpendicular;
-	rect[0].position = start + offset;
-	rect[1].position = end + offset;
-	rect[2].position = end - offset;
-	rect[3].position = start - offset;
-	rect[0].color = sf::Color::Red;
-	rect[1].color = sf::Color::Red;
-	rect[2].color = sf::Color::Red;
-	rect[3].color = sf::Color::Red;
+	sf::RenderWindow window(sf::VideoMode(width, height), "Slide");
+	CCanvas canvas(window);
 
 	while (window.isOpen())
 	{
@@ -33,16 +94,7 @@ int main()
 		}
 
 		window.clear();
-		window.draw(shape);
-
-		sf::Vertex line[] = {
-			sf::Vertex(sf::Vector2f(10, 10)),
-			sf::Vertex(sf::Vector2f(150, 150))
-		};
-
-		window.draw(rect);
-		window.draw(line, 2, sf::Lines);
-
+		slide->Draw(canvas);
 		window.display();
 	}
 
